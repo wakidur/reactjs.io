47d06d12c8620f6d54662f6c995702193ab37d2d
wakidur <wakidur@gmail.com>
2020-04-21 12:13:18 +0600

    todo app create

--- crash-course-todo-1.1.1/src/components/pages/todo/Todos.jsx
+++ crash-course-todo-1.1.1/src/components/pages/todo/Todos.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import TodoItem from './TodoItem';
+import AddTodo from './AddTodo';
 
 export class Todos extends Component {
   state = {
@@ -20,14 +21,53 @@ export class Todos extends Component {
   }
 
   // Toggle Component
-  markComplete = id => {
+  markComplete = (id) => {
     console.log(id);
-  }
+    this.setState({
+      todos: this.state.todos.map((item) => {
+        if (item.id === id) {
+          item.completed = !item.completed;
+        }
+        return item;
+      }),
+    });
+  };
 
   // Delete Todo
-  delTodo = id => {
+  delTodo = (id) => {
     console.log(id);
-  }
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((result) => {
+        console.log(result);
+        this.setState({
+          todos: [...this.state.todos.filter((item) => item.id !== id)],
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  // Add Todo
+  addTodo = (title) => {
+    console.log(title);
+    axios
+      .post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed: false,
+      })
+      .then((result) => {
+        console.log(result);
+        result.data.id = result.data.id += 1;
+        console.log(result.data);
+        this.setState({
+          todos: [...this.state.todos, result.data],
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   render() {
     return (
       <div className="row">
@@ -35,9 +75,21 @@ export class Todos extends Component {
           <h1>Todo List</h1>
         </div>
         <div className="col-12">
+          <AddTodo addTodo={this.addTodo} />
+        </div>
+        <div className="col-12">
           <ul className="list-group">
+          <li className="list-group-item d-flex justify-content-between align-items-center">
+            <p className=" mb-0 fr">Total Todo Item</p>
+            <span className="badge badge-primary badge-pill">{this.state.todos.length}</span>
+          </li>
             {this.state.todos.map((item) => (
-              <TodoItem key={item.id} todo={item} markComplete={this.markComplete} delTodo={this.delTodo} />
+              <TodoItem
+                key={item.id}
+                todo={item}
+                markComplete={this.markComplete}
+                delTodo={this.delTodo}
+              />
             ))}
           </ul>
         </div>
