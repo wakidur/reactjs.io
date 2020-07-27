53d787d643f86cda5f1e217cb2231164a1f92cc3
wakidur <wakidur@gmail.com>
2020-07-27 10:52:22 +0600

    version  1.1.1 start

---
+++ complete-react-developer/crwn-clothing-1.1.1/src/pages/homepage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Directory from '../../components/directory/Directory'
+import './HomePage.style.scss';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
+
+export default HomePage;
