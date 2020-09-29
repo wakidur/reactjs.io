c41891cc02aee55576630bd873dc474a1e54df46
Wakidur Rahman <wakidur@gmail.com>
2020-09-29 18:59:11 +0600

    redux-persist add to the project for session or local storage

--- complete-react-developer/crwn-clothing-redux-2.0.1/src/redux/store.js
+++ complete-react-developer/crwn-clothing-redux-2.0.1/src/redux/store.js
@@ -1,15 +1,20 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
-
-import RootReducer from "./root-reducer";
+import RootReducer from './root-reducer';
 
 // const middlewares = [logger];
 const middlewares = [thunk];
 
-const store = createStore( RootReducer, applyMiddleware(...middlewares));
+/**
+ * Befor adding redux-persist
+ * const store = createStore( RootReducer, applyMiddleware(...middlewares));
+ * export default store;
+ */
 
-export default store;
-
-
-
+/**
+ * redux-persist library use for local storag or session storage
+ */
+export const store = createStore(RootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
